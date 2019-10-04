3058cd8d40862704c2c66d0b0380a97dff3110d0
Angelique Nehmzow <angelique@nehmzow.com>
2019-10-04 14:43:57 -0700

    fill in save db function

--- database/index.js
+++ database/index.js
@@ -22,10 +22,31 @@ let repoSchema = mongoose.Schema({
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
-}
+let save = (repos) => {
+  for (let i = 0; i < repos.length; i++) {
+    var repo = repos[i];
+    var repoData = {
+      id: repo.id,
+      name: repo.name,
+      owner: {
+        login: repo.owner.login,
+        html_url: repo.owner.html_url,
+        repos_url: repo.owner.repos_url
+      },
+      html_url: repo.html_url,
+      description: repo.description,
+      created_at: repo.created_at,
+      updated_at: repo.updated_at,
+      popularity: {
+        watchers_count: repo.watchers_count,
+        stargazers_count: repo.stargazers_count,
+        forks_count: repo.forks_count
+      }
+    };
+    Repo.updateOne({id: repo.id}, repoData, {upsert: true}, (err, writeOpResult) => {
+      console.log(`repo '${repo.name}' saved to db`, writeOpResult);
+    });
+  }
+} // next: respond to client
 
 module.exports.save = save;
