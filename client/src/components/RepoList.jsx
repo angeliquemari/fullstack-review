8e1931328fa4cc526292fc2c8fb3e08411664d10
Angelique Nehmzow <angelique@nehmzow.com>
2019-10-05 12:12:01 -0700

    left pad numbers

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Repo = (props) => (
   <div>
-    <div>{props.order + 1}. <a href={props.repo.html_url}>{props.repo.name}</a> ({props.repo.owner_login}, {`${props.repo.updated_at.substring(0,10)} ${props.repo.updated_at.substring(11,19)}`}) {props.repo.description ? `- ${props.repo.description}`: null}</div>
+    <div>{(props.order + 1).toString().padStart(2, '0')}. <a href={props.repo.html_url}>{props.repo.name}</a> ({props.repo.owner_login}, {`${props.repo.updated_at.substring(0,10)} ${props.repo.updated_at.substring(11,19)}`}) {props.repo.description ? `- ${props.repo.description}`: null}</div>
   </div>
 );
 
